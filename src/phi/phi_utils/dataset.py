4cde389c8770bf83deff92e542855e770d4ccf44
derekhua777 <derekhua@ucla.edu>
2024-03-15 08:08:51 +0000

    zero_shot_evidence_prompt_transform

--- src/phi/phi_utils/dataset.py
+++ src/phi/phi_utils/dataset.py
@@ -21,7 +21,27 @@ class PhiPromptDataset(Dataset):
     # the following transform functions and __getitem__ fn, that you 
     # will use in def __getitem__ to convert a sample into prompt.
     # You can use the templates provided to in the constants.py file
-        
+    def zero_shot_eval_prompt_transform(self, idx):
+        prompt = PHI_ZERO_SHOT_EVAL_PROMPT.format(claim=self.data[idx]['claim'], task_type=self.data[idx]['task_type'])
+        return prompt
+    
+    def few_shot_eval_prompt_transform(self, idx):
+        prompt = PHI_FEW_SHOT_EVAL_PROMPT.format(claim=self.data[idx]['claim'], task_type=self.data[idx]['task_type'], examples=self.data[idx]['examples'])
+        return prompt
+    
+    
+    def zero_shot_evidence_prompt_transform(self, idx):
+        task_map = {"fairness": ("fair", "unfair"), "other": ("true", "false")}
+        supports_label = "SUPPORTS"
+        task_type = self.data[idx]['task_type']
+        task_type = task_type if task_type in task_map else "other"
+
+        information = "The claim is {}".format(task_map[task_type][0] if self.data[idx]['label'] == supports_label else task_map[task_type][1])
+
+        prompt = PHI_ZERO_SHOT_EVIDENCE_PROMPT.format(claim=self.data[idx]['claim'], information=information)
+        return prompt
+    
+    
     # End of TODO.
     ##################################################
     
@@ -34,11 +54,11 @@ class PhiPromptDataset(Dataset):
         # You may use if-else statements to choose the prompt
         # transform as per the prompt type given to you.
         if self.prompt_type == "zero_eval":
-            prompt = PHI_ZERO_SHOT_EVAL_PROMPT.format(claim=self.data[idx]['claim'], task_type=self.data[idx]['task_type'])
+            prompt = self.zero_shot_eval_prompt_transform(idx)
         elif self.prompt_type == "few_eval":
-            prompt = PHI_FEW_SHOT_EVAL_PROMPT.format(claim=self.data[idx]['claim'], task_type=self.data[idx]['task_type'], examples=self.data[idx]['examples'])
+            prompt = self.few_shot_eval_prompt_transform(idx)
         elif self.prompt_type == "zero_evidence":
-            prompt = PHI_ZERO_SHOT_EVIDENCE_PROMPT.format(claim=self.data[idx]['claim'], information=self.evidence_data[idx]['information'])
+            prompt = self.zero_shot_evidence_prompt_transform(idx)
         elif self.prompt_type == "zero_evidence_eval":
             prompt = PHI_ZERO_SHOT_EVIDENCE_EVAL_PROMPT.format(claim=self.data[idx]['claim'], evidence=self.evidence_data[idx]['evidence'], task_type=self.data[idx]['task_type'])
         else:
